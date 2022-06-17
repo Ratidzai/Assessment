045611523fb7568bdf3eac8af67d3ad8a7b18e1e
Ratidzai <80612340+Ratidzai@users.noreply.github.com>
2022-06-17 20:03:31 +0200

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,7 +1,7 @@
 counter = 3
 
 def tester():
-    global counter # This is the line I added for the outpu to run which is 4
+    global counter # This is the line I added for the output to run which is 4
     counter += 1
     print(counter)
 
